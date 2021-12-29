d09862744a973110a9125fdc61aad52631adf8f7
gursewakkular72 <gursewakkular72@gmail.com>
2021-12-28 20:52:35 -0800

    commit after finishing the project

---
+++ javaScript/script.js
@@ -0,0 +1,108 @@
+"use strict";
+
+let isMenuClicked = false;
+let windowWidth = 0;
+let year = document.querySelector(".year");
+let currentYear = new Date().getFullYear();
+const hero = document.querySelector(".hero-section");
+const header = document.querySelector(".main-header");
+const menuButton = document.querySelector(".mobile-menu-open");
+const menuCloseButton = document.querySelector(".mobile-menu-close");
+const websiteLogo = document.querySelector(".navigation-logo-list");
+const navigation = document.querySelector(".navigation");
+const mobileNavigation = document.querySelector(
+  ".mobile-nav-hidden-at-the-largest-screen"
+);
+
+const allLinks = document.querySelectorAll("a:link");
+
+// Implementing mobile navigation
+const showMobileNavigation = function () {
+  mobileNavigation.classList.add("mobile-navigation");
+  websiteLogo.classList.add("hidden");
+  navigation.classList.add("hidden");
+  menuCloseButton.classList.remove("hidden");
+  menuButton.classList.add("hidden");
+  isMenuClicked = true;
+};
+
+const removeMobileNavigation = function () {
+  mobileNavigation.classList.remove("mobile-navigation");
+  websiteLogo.classList.remove("hidden");
+  navigation.classList.remove("hidden");
+  menuCloseButton.classList.add("hidden");
+  menuButton.classList.remove("hidden");
+};
+
+const detectWidthChange = function () {
+  windowWidth = window.innerWidth;
+  removeMobileNavigationOnWidthChange();
+};
+
+window.addEventListener("resize", detectWidthChange);
+
+menuButton.addEventListener("click", function () {
+  showMobileNavigation();
+});
+
+menuCloseButton.addEventListener("click", function () {
+  removeMobileNavigation();
+});
+
+const removeMobileNavigationOnWidthChange = function () {
+  if (windowWidth > 864 && isMenuClicked) {
+    removeMobileNavigation();
+  }
+};
+
+// implementing smooth scrolling
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    const href = link.getAttribute("href");
+
+    if (href === "#") {
+      window.scrollTo({
+        top: 0,
+        behaviour: "smooth",
+      });
+
+      removeMobileNavigation();
+    }
+
+    if (href !== "#" && href.startsWith("#")) {
+      let element = document.querySelector(href);
+      element.scrollIntoView({ behavior: "smooth" });
+      console.log("this executes");
+    }
+    removeMobileNavigation();
+  });
+});
+
+// Implementing year
+
+year.textContent = currentYear;
+year.style.color = "inherit";
+year.style.fontWeight = "inherit";
+
+/// implementing sticky navigation
+
+const observer = new IntersectionObserver(
+  function (entries) {
+    console.log(entries[0].isIntersecting);
+    if (entries[0].isIntersecting) {
+      header.classList.remove("sticky");
+    }
+
+    if (!entries[0].isIntersecting) {
+      header.classList.add("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+
+observer.observe(hero);
